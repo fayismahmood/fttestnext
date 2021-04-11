eb0da60440e3136cb63195f0c0002b7c986bff69
fayismahmood <fmthottathil@gmail.com>
2021-04-11 17:24:13 +0530

    fgs

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,48 +1,21 @@
-import Styles from "../../styles/books.module.scss" 
-import Link from 'next/link'
+import React from 'react'
 
-function Books({books}) {
-  //const [BookList, setBookList] = useState(null)
-
-  return (
-  <div className={Styles._booksContainer}>
-    <nav>
-      <h2>Books</h2>
-      <ul>
-        <li>AboutMe</li>
-        <li>Blogs</li>
-      </ul>
-    </nav>
-    <div className={Styles._books}>
-    {
-      books.books.map((e,i)=>
-        (
-          <Link href={`./books/${i}`}>
-            <div className={Styles._eBook} key={e.isbn}>
-              <span> {e.title}</span>
-              <span> {e.isbn}</span>
-            </div>
-          </Link>
-
-        )
-        )
-      
+export default function Book({data}) {
+  //console.log(data);
+    return (
+        <div>
+            dfasfww
+        </div>
+    )
+}
+export const getServerSideProps = async (a) => {
+    const res = await fetch(`http://${a.req.headers.host}/api/books`)
+    console.log(a.req);
+    const data = await res.json()
+  
+    return {
+      props: {
+        data,
+      },
     }
-    </div>
- </div>  
-  )
-}
-
-export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/api/books')
-  const books = await res.json()
-
-  return {
-    props: {
-      books,
-    },
   }
-}
-
-
-export default Books
